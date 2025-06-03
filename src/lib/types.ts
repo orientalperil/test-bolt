6054fcbab9cb07dbcb692d2bb52acca10203b175
Kevin Tran <151680+orientalperil@users.noreply.github.com>
2025-06-02 21:57:37 -0700

    Ignore type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,51 +1,51 @@
 type PokemonCreateRequest = {
-  name: string,
-  number: string,
-  pokemonPhoto: string,
-  type: string,
-  description: string,
-  height: string,
-  weight: string,
-  maleGenderRatio: string,
-  femaleGenderRatio: string,
-  abilities: string,
-  eggGroups: string,
-  evolutions: string,
-  evolutionPhoto: string,
-  createdAt: Date,
-  updatedAt: Date,
+  name: string;
+  number: string;
+  pokemonPhoto: string;
+  type: string;
+  description: string;
+  height: string;
+  weight: string;
+  maleGenderRatio: string;
+  femaleGenderRatio: string;
+  abilities: string;
+  eggGroups: string;
+  evolutions: string;
+  evolutionPhoto: string;
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 type PokemonUpdateRequest = PokemonCreateRequest
 
 type PokemonResponse = {
   id: number;
-  name: string,
-  number: string,
-  pokemonPhoto: string,
-  pokemonPhotoUrl: string,
-  type: string,
-  description: string,
-  height: string,
-  weight: string,
-  maleGenderRatio: string,
-  femaleGenderRatio: string,
-  abilities: string,
-  eggGroups: string,
-  evolutions: string,
-  evolutionPhoto: string,
-  evolutionPhotoUrl: string,
-  createdAt: Date,
-  updatedAt: Date,
+  name: string;
+  number: string;
+  pokemonPhoto: string;
+  pokemonPhotoUrl: string;
+  type: string;
+  description: string;
+  height: string;
+  weight: string;
+  maleGenderRatio: string;
+  femaleGenderRatio: string;
+  abilities: string;
+  eggGroups: string;
+  evolutions: string;
+  evolutionPhoto: string;
+  evolutionPhotoUrl: string;
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 type ErrorResponse = {
-  error: string,
+  error: string;
 };
 
 type BlobResponse = {
-  pathname: string,
-  url: string,
+  pathname: string;
+  url: string;
 }
 
 export type { PokemonCreateRequest, PokemonUpdateRequest, PokemonResponse, ErrorResponse, BlobResponse }
